5b230f81deb03adf2489fd218cd7c697c1c9817a
ASiaka <alghenriyb@gmail.com>
2025-09-02 17:34:56 +0200

    mise en place page parcours

--- src/app/lib/data.js
+++ src/app/lib/data.js
@@ -1,110 +1,138 @@
 export default
 {
-  competences:
-    [
-      'HTML / CSS SCSS(SASS)',
-      'PHP (Orienté objet, MVC, API Rest, MySQL)',
-      'JavaScript vanilla',
-      'ReactJS, Redux',
-      'Photoshop',
-      'Illustrator',
-      'Indesign',
-      'Impression offset',
-      'Massicot',
-      'Pose de compteur gaz',
-    ],
-
-  formations: [
-    {
-      intitule: 'TP Développeur Web (niveau 5)',
-      organisme: 'Ecole O’clock',
-      periode: '28/03/22 - 08/09/22',
-    },
-
-    {
-      intitule: 'BAC PRO Production Imprimée',
-      organisme: 'GOBELINS l’école de l’Image',
-      periode: '2012 - 2015',
-    },
-
-    {
-      intitule: 'Formation offset, Massicot, PAO',
-      organisme: 'MÉDIAGRAF',
-      periode: '29/03/12 - 27/07/12',
-    },
-
-    {
-      intitule: 'Formation conducteur offset',
-      organisme: 'EPIGRAPH',
-      periode: '09/01/12 au 28/03/12',
-    },
-
-    {
-      intitule: 'Formation PAO',
-      organisme: 'CFA’com de Bagnolet',
-      periode: '21/03/11 - 29/06/11',
-    },
-
-    {
-      intitule: 'Formation PAO',
-      organisme: 'EPIGRAPH',
-      periode: '15/01/11 - 18/03/11',
-    },
-
-    {
-      intitule: 'CAP Signalétique Enseigne et Décor',
-      organisme: 'LP de Chirongui (MAYOTTE)',
-      periode: '2008 - 2010',
-    },
-  ],
-
   experiences: [
     {
-      intitule: 'Poseur de compteur gaz',
-      organisme: 'FRANCILIENNE MULTISERVICE',
-      periode: '09/10/23 - 31/08/25',
+      intitule: "Poseur de compteur gaz communicant",
+      periode: "09/10/2023 - 31/08/2025",
+      organisme: "Francilienne Multiservice - Saint-Denis 93",
+      taches1: "Installation de compteurs gaz communicants",
+      taches2: "Contrôle d'étanchéité et mise en service des appareils gaz",
     },
 
     {
-      intitule: 'Stage Développeur web - React',
-      organisme: 'AP Graphiste',
-      periode: '12/09/22 - 04/11/22',
+      intitule: "Stage Développeur web (React)",
+      periode: "12/09/2022 - 04/11/2022",
+      organisme: "AP Graphiste - Alfortville 94",
+      taches1: "Rédaction du cahier des charges",
+      taches2: "Refonte d'un site WordPress en React",
     },
 
     {
-      intitule: 'Poseur de compteur gaz',
-      organisme: 'GIE KAUNTARA',
-      periode: '23/10/17 - 28/02/23',
+      intitule: "Poseur de compteur gaz communicant",
+      periode: "23/10/2017 - 28/02/2023",
+      organisme: "GIE KAUNTARA - Chilly-Mazarin 91",
+      taches1: "Installation de compteurs gaz communicants",
+      taches2: "Contrôle d'étanchéité et mise en service des appareils gaz",
     },
 
     {
-      intitule: 'Chauffeur livreur',
-      organisme: 'PACKI EXPRESS',
-      periode: '01/03/17 - 02/05/17',
+      intitule: "Chauffeur livreur",
+      periode: "01/03/2017 - 02/05/2017",
+      organisme: "PACKI EXPRESS - Paris 19",
+      taches1: "Organisation de la tournée",
+      taches2: "Chargement du véhicule et livraison",
     },
 
     {
-      intitule: 'Manutentionnaire',
-      organisme: 'GEODIS',
-      periode: '17/10/16 - 29/11/16',
+      intitule: "Manutentionnaire",
+      periode: "17/10/2016 - 29/11/2016",
+      organisme: "GEODIS - Bonneuil-en-France 95",
+      taches1: "Mise en carton de produits",
+      taches2: "Déchargement et préparation de palettes",
     },
 
     {
-      intitule: 'Conducteur offset, massicotier',
-      organisme: 'SNCF',
-      periode: '01/09/15 - 05/02/16',
+      intitule: "Conducteur offset, massicotier",
+      periode: "01/09/2015 - 05/02/2016",
+      organisme: "SNCF Pôle édition courrier IDF - Paris 18",
+      taches1: "Préparation de la machine offset et impression",
+      taches2: "Découpe et mise en forme des impressions",
     },
 
     {
-      intitule: 'Apprenti conducteur offset',
-      organisme: 'SNCF',
-      periode: '02/09/13 - 28/08/15',
+      intitule: "Apprenti conducteur offset",
+      periode: "02/09/2013 - 28/08/2015",
+      organisme: "SNCF Pôle édition courrier IDF - Paris 18",
+      taches1: "Préparation de la machine offset et impression",
+      taches2: "Découpe et mise en forme des impressions",
     },
 
     {
-      intitule: 'Apprenti graphiste',
-      organisme: 'AVYS',
-      periode: '24/09/12 - 31/07/13',
+      intitule: "Apprenti graphiste",
+      periode: "24/09/2012 - 31/07/2013",
+      organisme: "AVYS Enseignes - Colombes 92",
+      taches1: "Création de logos et enseignes signalétiques",
+      taches2: "Retouche photo et mise en page de documents",
     },
   ],
+  
+  formations: [
+    {
+      intitule: "TP Développeur Web (niveau 5)",
+      periode: "28/03/2022 - 08/09/2022",
+      organisme: "Ecole O'clock",
+    },
+
+    {
+      intitule: "BAC PRO Production Imprimée",
+      periode: "2012 - 2015",
+      organisme: "GOBELINS l'école de l'Image",
+    },
+
+    {
+      intitule: "Formation offset, Massicot, PAO",
+      periode: "29/03/2012 - 27/07/2012",
+      organisme: "MÉDIAGRAF",
+    },
+
+    {
+      intitule: "Formation conducteur offset",
+      periode: "09/01/2012 au 28/03/2012",
+      organisme: "EPIGRAPH",
+    },
+
+    {
+      intitule: "Formation PAO",
+      periode: "21/03/2011 - 29/06/2011",
+      organisme: "CFA'com de Bagnolet",
+    },
+
+    {
+      intitule: "Formation PAO",
+      periode: "15/01/2011 - 18/03/2011",
+      organisme: "EPIGRAPH",
+    },
+
+    {
+      intitule: "CAP Signalétique Enseigne et Décor",
+      periode: "2008 - 2010",
+      organisme: "LP de Chirongui (MAYOTTE)",
+    },
+  ],
+
+  competences:
+    [
+      "HTML, CSS, JAVASCRIPT",
+      "REACT, NEXT.JS, SCSS (SASS)",
+      "ReactJS",
+      "PHOTOSHOP",
+      "ILLUSTRATOR",
+      "INDESIGN",
+      "IMPRESSION OFFSET",
+      "MASSICOT",
+      "POSE DE COMPTEUR GAZ COMMUNICANT",
+    ],
+
+  niveau:
+    [
+      "débutant",
+      "intermédiaire",
+      "confirmé",
+    ],
+
+  centresDInteret :
+    ["Vélo",
+      "Football",
+      "Programmation"
+    ],
 };
--- src/app/parcours/page.js
+++ src/app/parcours/page.js
@@ -1,14 +1,61 @@
 import styles from "@/app/parcours/parcours.module.scss";
+import parcoursData  from "../lib/data";
 
 export default function Parcours() {
 
     return (
         <main id="parcours" className={styles.parcours}>
-            <div className={styles.parcoursTop}>
-
+            <div className={styles.parcoursLeft}>
+                <div className={styles.contentLeft}>
+                    <h3 className={styles.experiences}>EXPERIENCES</h3>
+                    <div className={styles.experiencesContent}>
+                        {
+                            parcoursData.experiences.map((i, k) => {
+                                return (
+                                    <div className={styles.experienceBloc} key={k}>
+                                        <p className={styles.intitule}>{i.intitule}</p>
+                                        <p className={styles.periode}>{i.periode}</p>
+                                        <p className={styles.organisme}>{i.organisme}</p>
+                                        <p className={styles.taches1}>{i.taches1}</p>
+                                        <p className={styles.taches2}>{i.taches2}</p>
+                                    </div>
+                                        )
+                                    })
+                        }
+                    </div>
+                </div>
             </div>
-            <div className={styles.parcoursBottom}>
-
+            <div className={styles.parcoursRight}>
+                <div className={styles.contentRight}>
+                    <h3 className={styles.formations}>FORMATIONS</h3>
+                    <div className={styles.formationsContent}>
+                        {
+                            parcoursData.formations.map((i, k) => {
+                                return (
+                                    <div className={styles.formationBloc} key={k}>
+                                        <p className={styles.intitule}>{i.intitule}</p>
+                                        <p className={styles.periode}>{i.periode}</p>
+                                        <p className={styles.organisme}>{i.organisme}</p>
+                                        <p className={styles.taches1}>{i.taches1}</p>
+                                        <p className={styles.taches2}>{i.taches2}</p>
+                                    </div>
+                                        )
+                                    })
+                        }
+                    </div>
+                    <h3 className={styles.competences}>COMPETENCES</h3>
+                    <div className={styles.competencesContent}>
+                        <div className={styles.competencesBloc}>
+                            {
+                                parcoursData.competences.map((i, k) => {
+                                    return (
+                                        <p className={styles.intitule} key={k}>{i}</p>
+                                    )
+                                })
+                            }
+                        </div>
+                    </div>
+                </div>
             </div>
         </main>
     )
