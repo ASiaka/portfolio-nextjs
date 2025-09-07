f529f833d2d8a4664aeca9f3ebe936a7e4ff72b5
ASiaka <alghenriyb@gmail.com>
2025-09-07 21:51:56 +0200

    resolution pb polices

--- src/app/layout.js
+++ src/app/layout.js
@@ -9,6 +9,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="fr">
+      <head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100..900&display=swap"
+          rel="stylesheet"
+        />
+      </head>
       <body>
         {children}
       </body>
